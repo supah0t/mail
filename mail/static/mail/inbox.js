6571cda6c566606dc3f3cac6b2ce754f7b70fad9
supah0t <supah0t@users.noreply.github.com>
2020-11-16 17:51:28 +0200

    Project uploading

---
+++ mail/static/mail/inbox.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Use buttons to toggle between views
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#send').addEventListener('click', () => post_mail());
+
+  // By default, load the inbox
+  load_mailbox('inbox');
+});
+
+function compose_email() {
+
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+}
+
+function load_mailbox(mailbox) {
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  
+  fetch('/emails/inbox')
+.then(response => response.json())
+.then(email => {
+    // Print email
+    console.log(email);
+
+    // ... do something else with email ...
+});
+
+}
+
+function post_mail() {
+
+  //Posts mail to /emails route.
+  fetch('/emails', {
+    method: 'POST', 
+    body: JSON.stringify({
+      recipients: 'test@example.com',
+      subject: 'First Test',
+      body: 'This is a test'
+    })  
+  })
+  .then(response => response.json())
+  .then(result => {
+    //Print result
+    console.log(result);
+  });
+
+}
